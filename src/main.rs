da648ed204105e9a7810f3429504c7709324adbd
amirHosseinKhademii <sourena2575@gmail.com>
2023-03-18 23:04:17 +0100

    Add server and http modules

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,13 @@
+use http::request::Request;
+use server::Server;
+
+mod server;
+mod http {
+    pub mod method;
+    pub mod request;
+}
+
 fn main() {
     let server = Server::new("127.0.0.1:8080".to_string());
     server.run();
-}
-
-struct Server {
-    addr: String,
-}
-
-impl Server {
-    fn new(addr: String) -> Self {
-        Self { addr }
-    }
-
-    fn run(self) {
-        println!("Listening on {}", self.addr);
-    }
 }
