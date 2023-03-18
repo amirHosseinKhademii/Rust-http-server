2624e84e31327fcf84f8b598411be089b76b5e1b
amirHosseinKhademii <sourena2575@gmail.com>
2023-03-18 23:06:52 +0100

    Fix module exports

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,8 @@
 use http::request::Request;
 use server::Server;
 
+mod http;
 mod server;
-mod http {
-    pub mod method;
-    pub mod request;
-}
 
 fn main() {
     let server = Server::new("127.0.0.1:8080".to_string());
