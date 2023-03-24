f72e26618693d4a6cb26ea00a9a0e4261b9485ab
amirHosseinKhademii <sourena2575@gmail.com>
2023-03-24 20:52:08 +0100

    Add routing

--- src/web_handler.rs
+++ src/web_handler.rs
@@ -1,9 +1,17 @@
-use crate::http::{Request, Response, StatusCode};
+use crate::http::{Method, Request, Response, StatusCode};
 use crate::server::Handler;
 pub struct WebHandler;
 
 impl Handler for WebHandler {
-    fn handle_request(&mut self, _request: &Request) -> Response {
-        Response::new(StatusCode::OK, Some("<h1>It works!</h1>".to_string()))
+    fn handle_request(&mut self, request: &Request) -> Response {
+        match request.method() {
+            Method::GET => match request.path() {
+                "/" => Response::new(StatusCode::OK, Some("<h1>Index</h1>".to_string())),
+                "/hello" => Response::new(StatusCode::OK, Some("<h1>Hello</h1>".to_string())),
+                _ => Response::new(StatusCode::NotFound, None),
+            },
+
+            _ => Response::new(StatusCode::NotFound, None),
+        }
     }
 }
