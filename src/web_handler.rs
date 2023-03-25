0b846ed8d6e82903a13889c7f846e49bd4c7d14c
amirHosseinKhademii <sourena2575@gmail.com>
2023-03-25 12:28:59 +0100

    Serving html

--- src/web_handler.rs
+++ src/web_handler.rs
@@ -1,5 +1,6 @@
 use crate::http::{Method, Request, Response, StatusCode};
 use crate::server::Handler;
+use std::fs::read_to_string;
 pub struct WebHandler {
     public_path: String,
 }
@@ -8,14 +9,20 @@ impl WebHandler {
     pub fn new(public_path: String) -> Self {
         Self { public_path }
     }
+    fn read_file(&self, file_path: &str) -> Option<String> {
+        let path = format!("{}/{}", self.public_path, file_path);
+        read_to_string(path).ok()
+    }
 }
 
 impl Handler for WebHandler {
     fn handle_request(&mut self, request: &Request) -> Response {
         match request.method() {
             Method::GET => match request.path() {
-                "/" => Response::new(StatusCode::OK, Some("<h1>Index</h1>".to_string())),
-                "/hello" => Response::new(StatusCode::OK, Some("<h1>Hello</h1>".to_string())),
+                "/" => Response::new(StatusCode::OK, Some(self.read_file("index.html").unwrap())),
+                "/hello" => {
+                    Response::new(StatusCode::OK, Some(self.read_file("hello.html").unwrap()))
+                }
                 _ => Response::new(StatusCode::NotFound, None),
             },
 
